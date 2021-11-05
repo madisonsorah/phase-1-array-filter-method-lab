b6f8ea70950733bfc8a6616cfa66d77803b6c128
madison sorah <madisonsorah@gmail.com>
2021-11-05 14:59:14 -0400

    commit tree 1dfd6c8bd01915f38f545bde27398df8e2ec0134

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(listOfDrivers, firstName) {
+    return listOfDrivers.filter(function (driver){
+        return driver.toLowerCase() === firstName.toLowerCase()
+    });
+    }   
+
+
+    function fuzzyMatch(array, query) {
+        return array.filter(function (driver) {
+            return driver.slice(0, query.length) === query
+        })
+    }
+
+
+function matchName(obj, string) {
+    return obj.filter(function (nameMatch) {
+        return nameMatch.name === string;
+    })
+}
+
+
+
+// function superbowlResults(records) {
+//     const winRecord = records.find((record) => record.result === "W");
+//     if (winRecord) { 
+//         return winRecord.year; 
+//     }
+// }
